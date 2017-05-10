61db97b44a4ee75a7188c440eec8ad9a64b4de1e
DevonMars <devon_marsham_16@live.nl>
2017-05-10 13:24:11 +0200

    Button with intent naar MeldingScherm
    FloatActionButton gemaakt en het laten weergaven boven de map. Heb ook met een setonClicklistener een intent naar meldingscherm toegevoegd

--- app/src/main/java/com/example/justin/verbeterjegemeente/MainActivity.java
+++ app/src/main/java/com/example/justin/verbeterjegemeente/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.justin.verbeterjegemeente;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private SectionsPageAdapter mSectionsPageAdapter;
 
     private ViewPager mViewPager;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,50 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(), MeldingActivity.class);
+                startActivity(in);
+            }
+        });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        fab.show();
+
+                        break;
+
+                    case 1:
+                        fab.show();
+                        break;
+
+                    case 2:
+                        fab.hide();
+                        break;
+
+                    default:
+                        fab.hide();
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
 
         ServiceClient client = ServiceGenerator.createService(ServiceClient.class);

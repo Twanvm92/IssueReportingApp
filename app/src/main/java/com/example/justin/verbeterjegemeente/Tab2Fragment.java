53280f20ed97f4eef3e56a79c4ed74e08b8936a3
mika krooswijk <mikakrooswijk@live.nl>
2017-05-11 10:03:06 +0200

    Melding ID in database
    Melding ID in database

--- app/src/main/java/com/example/justin/verbeterjegemeente/Tab2Fragment.java
+++ app/src/main/java/com/example/justin/verbeterjegemeente/Tab2Fragment.java
@@ -3,18 +3,13 @@ package com.example.justin.verbeterjegemeente;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.database.DatabaseUtilsCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.example.justin.verbeterjegemeente.Database.DatabaseAccess;
 import com.example.justin.verbeterjegemeente.Database.DatabaseHanlder;
-
-import java.util.List;
 
 /**
  * Created by Justin on 27-4-2017.

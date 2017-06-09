d5a622d7ed3e8b39f35617f145ef4733908ee6dc
mika krooswijk <mikakrooswijk@live.nl>
2017-06-09 15:25:23 +0200

    comments

--- app/src/main/java/com/example/justin/verbeterjegemeente/Notification.java
+++ app/src/main/java/com/example/justin/verbeterjegemeente/Notification.java
@@ -27,10 +27,15 @@ public class Notification {
 
     public void makeNotification(Context context, String title, String text, ServiceRequest serviceRequest){
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+        // Sets the icon of the notification in the status bar.
         mBuilder.setSmallIcon(R.drawable.imageicon);
+        // Set the Title of the notification in the status bar.
         mBuilder.setContentTitle(title);
+        // Set the content of the notification.
         mBuilder.setContentText(text);
 
+
+        // Sets the intent to open when the notification is clicked, using the serivceRequest in the parameters.
         Intent i = new Intent(context, DetailedMeldingActivity.class);
         i.putExtra("serviceRequest", (Serializable) serviceRequest);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
@@ -59,14 +64,7 @@ public class Notification {
     }
 
 
-
-    public void clearNotifications(Context context){
-        for(String id :notificationIDs) {
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.cancel(Integer.parseInt(id));
-        }
-    }
-
+    // Creates a random number between 0 and 50 to give to the notification as the ID.
     public int getRandom(){
         Random rand = new Random();
         int  notificationID = rand.nextInt(50) + 1;

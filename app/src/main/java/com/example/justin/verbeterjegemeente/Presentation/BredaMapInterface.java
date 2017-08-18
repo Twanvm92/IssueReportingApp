44549b4c80f0cc140a4483b3420c600e5fd211b5
Twan van Maastricht <ajmaastr@avans.nl>
2017-08-18 18:55:12 +0200

    Deleted all Screen qualifiers for Layout files. Only one type of layout per layout exists now. Changed layout in DetailedMelding from Relative-  and Linearlayouts to one Constraintlayout. Added Upvote button in DetailedMelding layout. No functionality yet.

--- app/src/main/java/com/example/justin/verbeterjegemeente/Presentation/BredaMapInterface.java
+++ app/src/main/java/com/example/justin/verbeterjegemeente/Presentation/BredaMapInterface.java
@@ -2,6 +2,8 @@ package com.example.justin.verbeterjegemeente.Presentation;
 
 import android.webkit.JavascriptInterface;
 
+import org.json.JSONObject;
+
 /**
  * Created by twanv on 16-8-2017.
  */
@@ -9,5 +11,18 @@ import android.webkit.JavascriptInterface;
 public class BredaMapInterface {
 
     @JavascriptInterface
-    public void
+    public void goToDetailedMelding(JSONObject ServiceRequest) {
+
+    }
+
+
+    @JavascriptInterface
+    public void onCameraChanged(JSONObject LatLong) {
+
+    }
+
+    @JavascriptInterface
+    public void getMarkedLocation(JSONObject LatLong) {
+
+    }
 }

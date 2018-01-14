6e82b12f448a7c0dd40b7cae7f87c9254dee6d33
Twan van Maastricht <ajmaastr@avans.nl>
2018-01-14 18:36:26 +0100

    Added new viewmodel

---
+++ app/src/main/java/com/example/justin/verbeterjegemeente/viewModel/ServiceRequestListViewModel.java
@@ -0,0 +1,16 @@
+package com.example.justin.verbeterjegemeente.viewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by twanv on 14-1-2018.
+ */
+
+public class ServiceRequestListViewModel extends AndroidViewModel {
+
+    public ServiceRequestListViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
